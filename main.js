08b54ec80e21531589d8d8b4af743083742042aa
Emir Kabal <kabalemir@gmail.com>
2023-03-05 11:50:28 +0300

    added: set PlayerKilled to 0 and teleport stuff

--- main.js
+++ main.js
@@ -49,17 +49,34 @@ const collectSavesData = () => {
   profile = profiles[0];
 
   const savePath = forestPath + "Saves\\" + profile + "\\";
-  const importantFiles = ["GameStateSaveData.json", "SaveData.json"];
+  const importantFiles = [
+    "GameStateSaveData.json",
+    "PlayerStateSaveData.json",
+    "SaveData.json",
+  ];
 
   function fixFiles(name = "Singleplayer") {
     if (!fs.existsSync(savePath + name)) return;
     const saveFolders = fs.readdirSync(savePath + name);
-
+    const position = {
+      x: -1151.51819,
+      y: 138.793945,
+      z: -223.8553,
+    };
     saveFolders.forEach((saveFolder) => {
       const saveFiles = fs.readdirSync(savePath + name + "\\" + saveFolder);
+      saveFiles.sort((a, b) => {
+        if (a == importantFiles[0]) return -1;
+        if (b == importantFiles[0]) return 1;
+        if (a == importantFiles[1]) return -1;
+        if (b == importantFiles[1]) return 1;
+        if (a == importantFiles[2]) return -1;
+        if (b == importantFiles[2]) return 1;
+        return 0;
+      });
       saveFiles.forEach((saveFile) => {
-        console.log(saveFile);
         if (!importantFiles.includes(saveFile)) return;
+        console.log(name + "->" + saveFile);
         const saveData = fs.readFileSync(
           savePath + name + "\\" + saveFolder + "\\" + saveFile,
           "utf8"
@@ -78,8 +95,18 @@ const collectSavesData = () => {
           );
         } else if (importantFiles[1] == saveFile) {
           const defaultData = JSON.parse(saveData);
+          const data = getReadableJSON(defaultData.Data.PlayerState);
+          const savedPosition = data._entries.find(
+            (entry) => entry.Name === "player.position"
+          );
+          position.x = savedPosition.FloatArrayValue[0];
+          position.y = savedPosition.FloatArrayValue[1] + 5;
+          position.z = savedPosition.FloatArrayValue[2];
+        } else if (importantFiles[2] == saveFile) {
+          const defaultData = JSON.parse(saveData);
           const data = getReadableJSON(defaultData.Data.VailWorldSim);
           const robby = data.Actors.find((actor) => actor.TypeId === 9);
+          robby.Position = position;
           robby.State = 2;
           robby.StateFlags = 0;
           robby.Stats.Health = 100;
@@ -89,7 +116,13 @@ const collectSavesData = () => {
           robby.Stats.Fullness = 0;
           robby.Stats.Energy = 100;
 
+          const robbyKillStats = data.KillStatsList.find(
+            (killStats) => killStats.TypeId === 9
+          );
+          robbyKillStats.PlayerKilled = 0;
+
           const virginia = data.Actors.find((actor) => actor.TypeId === 10);
+          virginia.Position = position;
           virginia.State = 2;
           virginia.Stats.Health = 120;
           virginia.Stats.Anger = 0;
@@ -97,6 +130,11 @@ const collectSavesData = () => {
           virginia.Stats.Fullness = 0;
           virginia.Stats.Hydration = 0;
           virginia.Stats.Energy = 100;
+
+          const virginiaKillStats = data.KillStatsList.find(
+            (killStats) => killStats.TypeId === 10
+          );
+          virginiaKillStats.PlayerKilled = 0;
 
           defaultData.Data.VailWorldSim = getWritableJSON(data);
           fs.writeFileSync(
@@ -111,6 +149,10 @@ const collectSavesData = () => {
 
   try {
     fixFiles("Singleplayer");
+  } catch (error) {
+    console.log(error);
+  }
+  try {
     fixFiles("Multiplayer");
   } catch (error) {
     console.log(error);
